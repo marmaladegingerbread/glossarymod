7cda239f043400181f8f564db6898a49655abfab
Учитель <@pavlpku.kz>
2019-11-29 14:49:36 +0600

    change of concept

--- js/ang_func.js
+++
@@ -1,9 +0,0 @@
-//Only for AngularJS functions
-let app = angular.module("translate-app", []);
-
-app.controller("translate-ctrl", function($scope)
-{
-    $scope.ch_traslate = function(){
-        dynamicTranslate();            
-    }
-});
