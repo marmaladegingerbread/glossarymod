a4830272d2e99e83ecd5d9ebd105a3066f4c9da6
udlekov <udlekov@gmail.com>
2019-12-07 21:04:17 +0300

    2.0.4.1

--- js/translate.js
+++ js/translate.js
@@ -4,10 +4,28 @@
 * Rakhimov Danil©2019
 */
 //connect dictionaries and value
-let dictEn = glossaryEn;
-let dictRu = glossaryRu;
-let dictKz = glossaryKz;
+let dictEn = glossaryEn,
+  dictRu = glossaryRu,
+  dictKz = glossaryKz,
+  inValue = document.getElementById('inWords'),
+  outValueRu = document.getElementById('ruWord'),
+  outValueKz = document.getElementById('kzWord'),
+  outValueEn = document.getElementById('enWord');
+//functions
+function valueEqualRu(){
 
+}
+function valueEqualKz(){
+
+}
+function valueEqualEn(){
+
+}
+function checkLang(){
+  if(dictRu == dictRu) {
+    alert("hello");
+}
+}
 //change input
 let app = angular.module("glossaryApp", []);
 
